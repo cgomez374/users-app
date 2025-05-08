7feeb4713e18795f8362eef8bfc8c8467e5490c9
Carlos Gomez <CGomez374@yahoo.com>
2025-05-07 17:59:54 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Users App</h1>
+      <p>Grab random users from the RandomUsers API and filter them</p>
+    </>
+  )
+}
+
+export default App
